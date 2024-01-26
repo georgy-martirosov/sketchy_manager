e4830b3dbb758a55ceb062c49c1ea3b2a29bffbe
Goshka <ya.goshka@gmail.com>
2024-01-26 09:01:34 +0300

    cleanup

--- Sketchy_Manager.py
+++ Sketchy_Manager.py
@@ -1,4 +1,3 @@
-# Assuming you have not changed the general structure of the template no modification is needed in this file.
 from . import commands
 from .lib import fusion360utils as futil
 import adsk.core, adsk.fusion, adsk.cam, traceback
@@ -18,10 +17,8 @@ def run(context):
 
 def stop(context):
     try:
-        # Remove all of the event handlers your app has created
         futil.clear_handlers()
 
-        # This will run the start function in each of your commands as defined in commands/__init__.py
         commands.stop()
 
     except:
